17805051c184bc218debba6a556faa0181710571
NRK-496 <nishantrk28@protonmail.com>
2026-05-30 18:06:04 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo pacman -S --needed base-devel git
 
-sudo pacman -S nemo nemo-fileroller blender godot ghostty cmatrix lolcat gparted zed obs-studio starship kdeconnect qbittorrent
+sudo pacman -S nemo krita libresprite blender godot readest ghostty cmatrix lolcat gparted kclock zed obs-studio starship kdeconnect qbittorrent localsend fish
 
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
@@ -12,12 +12,6 @@ mv starship.toml ~/.config
 ./color-build.sh -f Mocha
 cd ..
 
-sh <(curl -L https://nixos.org/nix/install) --daemon
-
-git clone --depth 1 https://github.com/HyDE-Project/HyDE ~/HyDE
-cd ~/HyDE/Scripts
-./install.sh
-
 cd setup-for-arch
 mv Kurzgesagt_Wallpaper_Earth-with-Rings.png ~/.config/hyde/themes/"Catppuccin Mocha"/wallpapers/
 cd +5
@@ -25,6 +19,8 @@ cd +5
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
+cd ~/
+rm -rf paru
 
-paru -S zen-browser-bin zen-twilight-bin vesktop-bin cava extension-manager spotify spotify-adblock spicetify-cli obs-studio kwin-effects-forceblur cemu azahar ryujinx-git melonds-bin mgba-qt dolphin-emu-git wiiudownloader tty-clock pipes.sh 
+paru -S zen-browser-bin helium-browser vesktop-bin cava spotify-adblock spicetify-cli obs-studio kwin-effects-forceblur cemu azahar ryujinx-git melonds-bin mgba-qt dolphin-emu-git wiiudownloader tty-clock pipes.sh 
 rm -rf setup-for-arch
